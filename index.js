9c3aa3fd8319a231bd0a4bc5f308cec3de26fa6f
Amakris12 <71471809+Amakris12@users.noreply.github.com>
2020-11-12 19:12:08 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+var score = document.createElement('IMG');
+score.src = 'images/huff.jpeg';
+document.body.appendChild(score);
+score.setAttribute('id','img');
+
+var me = document.createElement('DIV');
+me.setAttribute( 'style','background-color:lightgray');
+var And = document.createElement('h1');
+var And2 = document.createTextNode("About me Andrew Makris");
+And.appendChild(And2);
+me.appendChild(And);
+document.body.appendChild(me);
+
+var me = document.createElement('DIV');
+me.setAttribute( 'style','background-color:lightgray');
+var And = document.createElement('P');
+var And2 = document.createTextNode("My name is Andrew Makris and i am currently 17 years old. I live in phoenix arizona and i have for my whole life.");
+And.appendChild(And2);
+me.appendChild(And);
+document.body.appendChild(me);
+
+var blue = document.createElement('DIV');
+blue.setAttribute( 'style','background-color:lightgray');
+var dark = document.createElement('P');
+var dark2 = document.createTextNode("I am currently a Senior in highschool and i go to Sandra Day Oconner Highschool. I have played football for my first three years but i stopped because i went to west mec to learn coding");
+dark.appendChild(dark2);
+blue.appendChild(dark);
+document.body.appendChild(blue);
+
+var me = document.createElement('DIV');
+me.setAttribute( 'style','background-color:lightgray');
+var And = document.createElement('P');
+var And2 = document.createTextNode("I am currently working at Panera bread as a dishwasher and a service attendent out in the dining room");
+And.appendChild(And2);
+me.appendChild(And);
+document.body.appendChild(me);
+
+var me = document.createElement('DIV');
+me.setAttribute( 'style','background-color:lightgray');
+var And = document.createElement('P');
+var And2 = document.createTextNode("I just broke things off with my girlfriend for the past year and a half but its okay because i have amaizng friends");
+And.appendChild(And2);
+me.appendChild(And);
+document.body.appendChild(me);
+
+var me = document.createElement('DIV');
+me.setAttribute( 'style','background-color:lightgray');
+var And = document.createElement('P');
+var And2 = document.createTextNode("I am currently planning on atteneding Glendale Community college for the first two years of college. After that i will either be heading too ASU or GCU.");
+And.appendChild(And2);
+me.appendChild(And);
+document.body.appendChild(me);
+
+
+var man = document.createElement('IMG');
+man.src = 'images/G.jpeg';
+document.body.appendChild(man);
+man.setAttribute('id','right');
